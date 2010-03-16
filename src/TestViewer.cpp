d46b1c4276f7f84b1c773f5086bf50152fdd340e
Christopher Gaudig <cgaudig@gaudig-u.dfki.uni-bremen.de>
2010-03-16 15:48:16 +0100

    removed avt guppy dependency from testviewer

--- src/TestViewer.cpp
+++ src/TestViewer.cpp
@@ -30,9 +30,8 @@
 #include <iostream>
 #include <highgui.h>
 #include <cv.h>
-//#include "../../prosilica/GigE/CamGigEProsilica.h"
 #include "CamFireWire.h"
-#include "../../camera_avt_guppy/AVT_Guppy.h"
+//#include "../../camera_avt_guppy/src/AVT_Guppy.h"
 #include <libraw1394/raw1394.h>
 
 using namespace camera;
@@ -55,19 +54,20 @@ void plotCameras ( std::vector<CamInfo> &cam_infos)
 
 int main(int argc, char**argv)
 {
-bool stereo = false;
+    bool stereo = false;
 
     Frame left_frame;
     Frame right_frame;
 
-    frame_size_t size(752,480);
+    //frame_size_t size(752,480);
+    frame_size_t size(640,480);
 
+    
     left_frame.init(size.width,size.height,3,MODE_BAYER_RGGB,false);
     if(stereo) right_frame.init(size.width,size.height,3,MODE_BAYER_RGGB,false);
 
-
-    AVT_Guppy left_camera;
-    AVT_Guppy right_camera;
+    CamFireWire left_camera;
+    CamFireWire right_camera;
     CamInterface &left_cam = left_camera;
     CamInterface &right_cam = right_camera;
 
@@ -77,15 +77,18 @@ bool stereo = false;
     std::cerr << "cam.isOpen = " << left_cam.isOpen() << std::endl;
     cvWaitKey(100);
     left_cam.open(cam_infos[0], Master);
-    cvWaitKey(100);
+    left_cam.setAttrib(camera::int_attrib::IsoSpeed, 400);
+  
     if(stereo) right_cam.open(cam_infos[1], Monitor);
+    if(stereo) left_cam.setAttrib(camera::int_attrib::IsoSpeed, 200);
+    if(stereo) right_cam.setAttrib(camera::int_attrib::IsoSpeed, 200);
+
     cvWaitKey(100);
-    //showCamInfos(cam_infos);
     std::cerr << "cam.isOpen = " << left_cam.isOpen() << std::endl;
     frame_size_t fs;
   
     fs.height = 480;
-    fs.width = 752;
+    fs.width = 640; //fs.width = 752;
     cv::namedWindow("left",1);
     if(stereo) cv::namedWindow("right",1);
     cvMoveWindow("left",10,10);
@@ -99,6 +102,7 @@ bool stereo = false;
     left_cam.setAttrib(int_attrib::ExposureValue, 300);
     left_cam.setAttrib(int_attrib::WhitebalValueBlue, 580);
     left_cam.setAttrib(int_attrib::WhitebalValueRed, 650);
+    left_cam.setAttrib(int_attrib::AcquisitionFrameCount, 200);
 
     if(stereo)
     {
@@ -108,107 +112,35 @@ bool stereo = false;
         right_cam.setAttrib(int_attrib::ExposureValue,300);
         right_cam.setAttrib(int_attrib::WhitebalValueBlue, 580);
         right_cam.setAttrib(int_attrib::WhitebalValueRed, 650);
+	right_cam.setAttrib(int_attrib::AcquisitionFrameCount, 200);
     }
-
-    dc1394_video_set_framerate(left_camera.dc_camera, DC1394_FRAMERATE_60);
 
     cvWaitKey(50);
     timeval ts, te;
     
     gettimeofday(&ts,NULL);
-    std::cerr << "grabbing...";
-                      left_cam.grab(SingleFrame,1);
-                      //right_cam.grab(SingleFrame,1);
-        std::cerr << "broadcasting...";
 
-        left_camera.broadcastGrabCommand();                  //  left_cam.grab(SingleFrame,1);
-        cvWaitKey(50);
-        left_camera.broadcastGrabCommand();                  //  left_cam.grab(SingleFrame,1);
-        cvWaitKey(50);
-
-
-        for(int i = 0 ; i < 50 ; i++)
-        {
-            left_camera.broadcastGrabCommand();
-            cvWaitKey(40);
-        }
-
-
-    for(int i = 0 ; i < 50 ; i++)
+    left_cam.setAttrib(camera::double_attrib::FrameRate,30);
+    if(stereo) right_cam.setAttrib(camera::double_attrib::FrameRate,30);
+    
+    left_camera.clearBuffer();
+    if(stereo) right_camera.clearBuffer();
+    
+    for(int i = 0 ; i< 200 ; i++)
     {
-        //timeval t1; gettimeofday(&t1,NULL); std::cerr << "before grab " << t1.tv_sec%1000 + t1.tv_usec/1000000.0 << std::endl;
-
-        //timeval t2; gettimeofday(&t2,NULL); std::cerr << "before grab " << t2.tv_sec%1000 + t2.tv_usec/1000000.0 << std::endl;
-
-        //timeval t3; gettimeofday(&t3,NULL); std::cerr << "before grab " << t3.tv_sec%1000 + t3.tv_usec/1000000.0 << std::endl;
-        //std::cerr << "retrieving frame..." << std::endl;
-
-        std::cerr << "broadcast...";
-
-                          //  left_cam.grab(SingleFrame,1);
-
-        //timeval t4; gettimeofday(&t4,NULL); std::cerr << "before grab " << t4.tv_sec%1000 + t4.tv_usec/1000000.0 << std::endl;
-                std::cerr << "retr right...";
-        if(stereo) right_cam.retrieveFrame(right_frame, 1000);
-                  //      right_cam.grab(SingleFrame,1);
-                std::cerr << "retr left...";
-
-                left_cam.retrieveFrame(left_frame, 1000);
-
-
-
-//cvWaitKey(230);
-
-        //timeval t5; gettimeofday(&t5,NULL); std::cerr << "before grab " << t5.tv_sec%1000 + t5.tv_usec/1000000.0 << std::endl;
-
-        cv::imshow("left",left_frame.convertToCvMat());
-        if(stereo) cv::imshow("right",right_frame.convertToCvMat());
-
-        //std::cerr << "image showed" << std::endl;
-        cvWaitKey(50);
-
+	left_cam.grab(SingleFrame, 10);
+        std::cerr << "retrieving..." << std::endl;
+	left_cam.retrieveFrame(left_frame,0);
+	if(stereo) right_cam.retrieveFrame(right_frame,0);;
+	imshow("left",left_frame.convertToCvMat());
+	if(stereo) imshow("right", right_frame.convertToCvMat());
+	cvWaitKey(2);
     }
     
     gettimeofday(&te,NULL);
-
     std::cerr << (1000000*te.tv_sec+te.tv_usec - 1000000*ts.tv_sec-ts.tv_usec)/1000000.0 << " seconds" << std::endl;
-
     cvWaitKey();
 
-    //plotCameras(cam_infos);
-/*
-    try
-    {
-        //opens a specific camera
-        tCamInfo info;
-        info.display_name = "GE1900C";
-        std::cout << "open camera " << info.display_name << "\n";
-        camera.open2(info);
-        
-        //configure the camera to match the frame;
-        camera.setFrameSettings(frame);
-
-        //start capturing
-        camera.grab(eContinuously,10);
-
-        //display 100 frames
-        int i=0;
-        while(i<100)
-        {
-            if (camera.isFrameAvailable())
-            {
-                camera >> frame;
-                //convert frame to openCV
-                cv::Mat img(size.height,size.width, CV_8UC3, frame.getImagePtr());
-                cv::imshow("image",img);
-                i++;
-            }
-            cv::waitKey(2);
-        }
-    }
-    catch(std::runtime_error e)
-    { std::cout <<  e.what() << std::endl << std::endl;}
-*/
     //close camera
     left_cam.close();
     if(stereo) right_cam.close();
