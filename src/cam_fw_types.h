7024cd632f38662c34d05a992bd51673abb64aa2
Christopher Gaudig <christopher.gaudig@dfki.de>
2011-01-07 14:41:17 +0100

    added missing header cam_fw_types.h

---
+++ src/cam_fw_types.h
@@ -0,0 +1,21 @@
+#ifndef CAM_FW_TYPES_H
+#define CAM_FW_TYPES_H
+
+namespace camera {
+
+ struct CalibrationData
+      {
+	float cx;
+	float cy;
+	float k1;
+        float k2;
+        float p1;
+        float p2;
+        float k3;
+      };
+
+       
+
+}
+
+#endif
